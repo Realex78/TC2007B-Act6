b46f1385223784d1689cf9f6d26fb3fb26a2e78e
Realex78 <36749154+Realex78@users.noreply.github.com>
2025-08-25 12:14:09 -0600

    Initial Commit

---
+++ rPhotographs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rPhotographs
+//
+//  Created by Alumno on 25/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rPhotographs/rPhotographsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rPhotographsApp.swift
+//  rPhotographs
+//
+//  Created by Alumno on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct rPhotographsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
